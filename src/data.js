87a338700e535626b5423c0e895c934d5455ec9e
JMMFL <mehravarj+github@gmail.com>
2022-08-17 11:39:46 -0400

    Changed property name

--- src/data.js
+++ src/data.js
@@ -4,18 +4,18 @@ const data = {
     "task-2": { id: "task-2", content: "Wash Dishes" },
     "task-3": { id: "task-3", content: "Eat Apples" },
     "task-4": { id: "task-4", content: "Learn React" },
-    "task-5": { id: "task-5", content: "Have Fun" }
+    "task-5": { id: "task-5", content: "Have Fun" },
   },
 
   columns: {
     "column-1": {
       id: "column-1",
-      content: "To Do",
-      taskIds: ["task-1", "task-2", "task-3", "task-4"]
-    }
+      title: "To Do",
+      taskIds: ["task-1", "task-2", "task-3", "task-4"],
+    },
   },
 
-  columnOrder: [`column-1`]
+  columnOrder: [`column-1`],
 };
 
 export default data;
