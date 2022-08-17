238d6ec3a74294149e2c078ba0f2c28a95953fc7
JMMFL <mehravarj+github@gmail.com>
2022-08-17 13:39:40 -0400

    Added fist column of tasks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import "./styles.css";
+import { useState } from "react";
+import Column from "./components/Column";
+import inital from "./data";
 
 export default function App() {
-  return "Hello World";
+  const [data, setData] = useState(inital);
+
+  return data.columnOrder.map((columnId) => {
+    const column = data.columns[columnId];
+    const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
+    return <Column key={column.id} column={column} tasks={tasks} />;
+  });
 }
