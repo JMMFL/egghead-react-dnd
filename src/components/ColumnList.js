3f4f0add32e5ae3c9ea7c8043ddc48a6120c8f5b
JMMFL <mehravarj+github@gmail.com>
2022-08-28 16:50:58 -0400

    Added key property

--- src/components/ColumnList.js
+++ src/components/ColumnList.js
@@ -4,6 +4,13 @@ export default function ColumnList({ tasks, columns, columnOrder }) {
   return columnOrder.map((columnId) => {
     const column = columns[columnId];
     const columnTasks = column.taskIds.map((taskId) => tasks[taskId]);
-    return <Column id={column.id} tasks={columnTasks} column={column} />;
+    return (
+      <Column
+        key={column.id}
+        id={column.id}
+        tasks={columnTasks}
+        column={column}
+      />
+    );
   });
 }
