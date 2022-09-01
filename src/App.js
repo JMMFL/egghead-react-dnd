76b0fe683d57f329661f80e9b35a3b10530faaf0
JMMFL <mehravarj+github@gmail.com>
2022-09-01 19:10:19 -0400

    Created onDragEnd to save list changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,34 @@ import inital from "./data";
 export default function App() {
   const [data, setData] = useState(inital);
 
-  const onDragEnd = (result) => console.log(result);
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+    const { droppableId: desDropId, index: desIndex } = destination;
+    const { droppableId: souDropId, index: souIndex } = source;
+
+    if (!destination) return;
+    if (desDropId === souDropId && desIndex === souIndex) return;
+
+    const column = data.columns[souDropId];
+    const newTaskIds = [...column.taskIds];
+    newTaskIds.splice(souIndex, 1);
+    newTaskIds.splice(desIndex, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newData = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    setData(newData);
+  };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
